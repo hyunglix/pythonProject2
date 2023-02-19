1cc6a9b3c84a201ad2ac79cc0a6a76225fb0306a
Professional <stella.mamikonian@gmail.com>
2023-02-19 09:04:52 +0200

    job

---
+++ parent_chid.py
@@ -0,0 +1,37 @@
+# class Wow:
+#     def __wow(self):
+#         print('Wow')
+#     def _hello(self):
+#         print('Hello')
+#
+# some_obj = Wow()
+# some_obj._hello()
+# some_obj._Wow__wow()
+
+
+
+# class Hello:
+#     def __init__(self):
+#         print('Hello')
+# class Hello_World(Hello):
+#     def __init__(self):
+#         super().__init__()
+#         print('World!')
+# hello_world=Hello_World()
+
+
+
+class Grandparent:
+    def about(self):
+        print('I am Grandparent')
+    def about_myself(self):
+        print('I am Grandparent')
+class Parent(Grandparent):
+    def about_myself(self):
+        print('I am parent')
+class Child(Parent):
+    def __init__(self):
+        super().about()
+        super().about_myself()
+nick=Child()
+
